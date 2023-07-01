45c6774f39103fa577e7fe5dc7cf7828c44a763c
HusseinMansourMohd <husseinmcwii13@gmail.com>
2023-07-01 09:30:15 -0500

    topic1

--- topic_1.py
+++ topic_1.py
@@ -3,3 +3,29 @@ print(myList)
 
 for i in myList:
     print(i)
+
+if "bana" in my List:
+    print("yes")
+else:
+    print("no")
+
+#append
+myList.append("Lemon") 
+
+
+#pop : return last item and then removes it.
+
+Last_item = myList.pop()
+
+#remove specific element methods 
+
+myList = mylist.remove("apple")
+
+#Delete all items from the list.
+myList.clear()
+
+printLisy 
+
+myList = [-3, 1, 4, 5, -6]
+
+myList.sort()
