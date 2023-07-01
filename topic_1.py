c980727ec5416e11090ad35618f87b0a68dd1702
HusseinMansourMohd <husseinmcwii13@gmail.com>
2023-07-01 11:24:03 -0500

    2.

--- topic_1.py
+++ topic_1.py
@@ -1,11 +1,13 @@
 
-item1 = myList[-1] #print the last 
+myList = ["bannana", "apple" , "cherry"]
+
+item1 = myList[-1] #print the last element
 print(myList)
 
 for i in myList:
     print(i)
 
-if "bana" in my List:
+if "bana" in myList:
     print("yes")
 else:
     print("no")
@@ -29,6 +31,30 @@ myList = [-3, 1, 4, 5, -6]
 
 myList.sort() # or sorted(myList)
 
+#copy list and slicing:
+list_org = [1, 2, 3]
+list_cpy = list_org[:]
+
 #Lists: ordered, mutable, allows duplicate elements
 myList = [0] * 5
-myList = [1,2,3,4,5,6]
+myList2 = [1,2,3,4,5,6]
+myList3 = myList + myList2
+
+a = myList[1:5]
+a = myList[::1] #from begining to the end use one step
+
+a = myList[::-1] # reverse order a list using only one step at a time
+
+print(a)
+
+###
+list_org = ["banna", "cherry", "apple"]
+
+List_cpy = list_org.copy()
+List_cpy = list_org[:] # slicing
+List_cpy.append("lemon")
+
+
+print(list_cpy)
+print(list_org)
+
